b074e8ee9eadb73c1a73ed88527a829af793c69c
Lucas Dolabela <166764598+Dolabelaa@users.noreply.github.com>
2024-09-19 14:38:18 -0300

    Create Veiculo.java

---
+++ codigo/Veiculo.java
@@ -0,0 +1,45 @@
+package codigo;
+
+public class Veiculo
+{
+    private String placa;
+    private Cliente cliente;
+
+    public Veiculo(String placa)
+    {
+        this.placa = placa;
+    }
+
+    public void adicionarCliente(Cliente cliente)
+    {
+        this.cliente = cliente;
+    }
+
+    public String getPlaca()
+    {
+        return placa;
+    }
+
+    public void setPlaca(String placa)
+    {
+        this.placa = placa;
+    }
+
+    public Cliente getCliente()
+    {
+        return cliente;
+    }
+
+    public void operation()
+    {
+        System.out.println("Operação realizada para o veículo de placa: " + placa);
+        if (cliente != null)
+        {
+            System.out.println("Cliente associado: " + cliente.getNome());
+        }
+        else
+        {
+            System.out.println("Nenhum cliente associado.");
+        }
+    }
+}

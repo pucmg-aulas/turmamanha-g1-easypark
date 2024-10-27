eefda67e4add0b0b12ce7931edc25a32a2f52607
Pedro Eduarro Pimenta <161333099+pedroedupimenta@users.noreply.github.com>
2024-10-27 19:15:26 -0300

    Create Pagamento.java

---
+++ src/test/java/Models/Pagamento.java
@@ -0,0 +1,21 @@
+package models
+
+public class Pagamento {
+    private int id;
+    private String cliente;
+    private double valor;
+
+    public Pagamento(int id, String cliente, double valor) {
+        this.id = id;
+        this.cliente = cliente;
+        this.valor = valor;
+    }
+
+    public int getId() { return id; }
+    public String getCliente() { return cliente; }
+    public double getValor() { return valor; }
+
+    public void setId(int id) { this.id = id; }
+    public void setCliente(String cliente) { this.cliente = cliente; }
+    public void setValor(double valor) { this.valor = valor; }
+}

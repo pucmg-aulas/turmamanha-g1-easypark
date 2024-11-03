75d8a1daa328e2c5cc199ba7d3bde624aeafed3a
Kaua Agner <kauaag6@gmail.com>
2024-11-03 20:43:57 -0300

    Filtro Historico Uso

--- src/test/java/Controllers/ExibirHistoricoUsoController.java
+++ src/test/java/Controllers/ExibirHistoricoUsoController.java
@@ -1,4 +1,3 @@
-
 package Controllers;
 
 import Models.Estacionamento;
@@ -9,13 +8,17 @@ import Models.Veiculo;
 import dao.EstacionamentoDAO;
 import dao.PagamentoDAO;
 import dao.VeiculoDAO;
+import java.time.LocalDate;
+import java.io.FileNotFoundException;
 import view.ExibirHistoricoUsoView;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.List;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Iterator;
 
 import javax.swing.JDesktopPane;
@@ -23,16 +26,15 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JScrollPane;
 
-
-
 public class ExibirHistoricoUsoController {
+
     private ExibirHistoricoUsoView view;
     private JDesktopPane desktopPane;
     private PagamentoDAO pagamentos;
     private String clienteCpf;
     private VeiculoDAO veiculos;
     private EstacionamentoDAO estacionamentos;
-    
+
     public ExibirHistoricoUsoController(JDesktopPane desktopPane, String cpf) throws IOException {
         this.view = new ExibirHistoricoUsoView(desktopPane);
         this.pagamentos = PagamentoDAO.getInstance();
@@ -40,47 +42,35 @@ public class ExibirHistoricoUsoController {
         this.estacionamentos = EstacionamentoDAO.getInstance();
         this.desktopPane = desktopPane;
         this.clienteCpf = cpf;
-        
+
         desktopPane.add(view);
         this.view.setVisible(true);
-                
+
         carregarHistoricoCliente();
+        
+        this.view.getFiltrarBtn().addActionListener(e -> {
+            String dataInicio = view.getDataInicio(); 
+            String dataFim = view.getDataFim();
+            filtrarHistoricoPorData(dataInicio, dataFim);
+        });
     }
-    
-     public JScrollPane getScrollPane() {
+
+    public JScrollPane getScrollPane() {
         return view.getScrollPane();
     }
-    
-      private void carregarHistoricoCliente() {
-        Object colunas[] = { "CPF", "Nome Estacionamento", "Tipo Vaga", "Placa Veículo", "Tempo de Permanência (min)", "Valor(R$)" };
+
+    private void carregarHistoricoCliente() {
+        Object colunas[] = {"CPF", "Nome Estacionamento", "Tipo Vaga", "Placa Veículo", "Tempo de Permanência (min)", "Valor(R$)"};
         DefaultTableModel tm = new DefaultTableModel(colunas, 0);
 
         try {
-            // Lê todos os históricos
             List<Pagamento> todosPagamentos = pagamentos.getPagamentosPorCpf(clienteCpf);
-            
+
             if (todosPagamentos.isEmpty()) {
                 JOptionPane.showMessageDialog(view, "Nenhum histórico encontrado para o CPF: " + clienteCpf);
             } else {
                 for (Pagamento pagamento : todosPagamentos) {
-                    String placaVeiculo = pagamento.getPlacaVeiculo();
-                    Veiculo veiculoAtual = veiculos.buscarVeiculoPorPlaca(placaVeiculo);
-                    String cpfAtual = veiculoAtual.getCliente().getCpf();
-                    Estacionamento estacionamentoAtual = estacionamentos.getEstacionamentoPorId(pagamento.getIdEstacionamento());
-                    String estacionamentoNome = estacionamentoAtual.getNome();
-                    ITipo vaga = pagamento.getTipoVaga();
-                    String tipoVaga = vaga.getTipo();
-                    int tempoTotal = pagamento.getTempoTotal();
-                    double valorTotal = pagamento.getValorPago();
-                    
-                    String[] linha = new String[6];
-                    linha[0] = cpfAtual;
-                    linha[1] = estacionamentoNome;
-                    linha[2] = tipoVaga;
-                    linha[3] = placaVeiculo;
-                    linha[4] = String.valueOf(tempoTotal);
-                    linha[5] = String.valueOf(valorTotal);
-                    tm.addRow(linha);
+                    adicionarHistoricoNaTabela(tm, pagamento);
                 }
             }
             view.getTableHistorico().setModel(tm);
@@ -88,5 +78,80 @@ public class ExibirHistoricoUsoController {
             JOptionPane.showMessageDialog(view, "Erro ao carregar histórico: " + e.getMessage());
         }
     }
+
+    private void adicionarHistoricoNaTabela(DefaultTableModel tm, Pagamento pagamento) throws FileNotFoundException {
+        String placaVeiculo = pagamento.getPlacaVeiculo();
+        Veiculo veiculoAtual = veiculos.buscarVeiculoPorPlaca(placaVeiculo);
+        String cpfAtual = veiculoAtual.getCliente().getCpf();
+        Estacionamento estacionamentoAtual = estacionamentos.getEstacionamentoPorId(pagamento.getIdEstacionamento());
+        String estacionamentoNome = estacionamentoAtual.getNome();
+        ITipo vaga = pagamento.getTipoVaga();
+        String tipoVaga = vaga.getTipo();
+        int tempoTotal = pagamento.getTempoTotal();
+        double valorTotal = pagamento.getValorPago();
+
+        String[] linha = new String[6];
+        linha[0] = cpfAtual;
+        linha[1] = estacionamentoNome;
+        linha[2] = tipoVaga;
+        linha[3] = placaVeiculo;
+        linha[4] = String.valueOf(tempoTotal);
+        linha[5] = String.valueOf(valorTotal);
+        tm.addRow(linha);
+    }
+
+    public void filtrarHistoricoPorData(String dataInicioStr, String dataFimStr) {
+        try {
+            LocalDate dataInicio = null;
+            LocalDate dataFim = null;
+
+            if (!dataInicioStr.isEmpty()) {
+                dataInicio = LocalDate.parse(dataInicioStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+
+            if (!dataFimStr.isEmpty()) {
+                dataFim = LocalDate.parse(dataFimStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
+
+            if (dataInicio == null && dataFim == null) {
+                carregarHistoricoCliente();
+                return;
+            }
+
+            if ((dataInicio == null && dataFim != null) || (dataInicio != null && dataFim == null)) {
+                JOptionPane.showMessageDialog(view, "Por favor, preencha ambos os campos de data ou nenhum.");
+                return;
+            }
+
+            List<Pagamento> todosPagamentos = pagamentos.getPagamentosPorCpf(clienteCpf);
+
+            final LocalDate finalDataInicio = dataInicio;
+            final LocalDate finalDataFim = dataFim;
+
+            List<Pagamento> pagamentosFiltrados = todosPagamentos.stream()
+                    .filter(pagamento -> {
+                        LocalDateTime dataUso = pagamento.getDataPagamento();
+                        return (dataUso.toLocalDate().isEqual(finalDataInicio) || dataUso.toLocalDate().isAfter(finalDataInicio))
+                                && (dataUso.toLocalDate().isEqual(finalDataFim) || dataUso.toLocalDate().isBefore(finalDataFim));
+                    })
+                    .toList();
+
+            DefaultTableModel tm = new DefaultTableModel(new Object[]{"CPF", "Nome Estacionamento", "Tipo Vaga", "Placa Veículo", "Tempo de Permanência (min)", "Valor(R$)"}, 0);
+            if (pagamentosFiltrados.isEmpty()) {
+                JOptionPane.showMessageDialog(view, "Nenhum histórico encontrado para o intervalo de datas.");
+            } else {
+                for (Pagamento pagamento : pagamentosFiltrados) {
+                    adicionarHistoricoNaTabela(tm, pagamento);
+                }
+            }
+            view.getTableHistorico().setModel(tm);
+
+        } catch (DateTimeParseException e) {
+            JOptionPane.showMessageDialog(view, "Formato de data inválido. Utilize o formato dd/MM/yyyy.");
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(view, "Erro ao carregar histórico: " + e.getMessage());
+        }
+    }
+
+
 }
- 
--- src/test/java/view/ExibirHistoricoUsoView.java
+++ src/test/java/view/ExibirHistoricoUsoView.java
@@ -1,6 +1,8 @@
 
 package view;
 
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -28,6 +30,18 @@ public class ExibirHistoricoUsoView extends javax.swing.JInternalFrame {
         return scrollPane;
     }
     
+    public String getDataInicio() {
+        return DataInicio.getText();
+    }
+
+    public String getDataFim() {
+        return DataFim.getText();
+    }
+    
+    public JButton getFiltrarBtn() {
+        return filtrarBtn;
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -37,6 +51,11 @@ public class ExibirHistoricoUsoView extends javax.swing.JInternalFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaHistorico = new javax.swing.JTable();
+        DataFim = new javax.swing.JTextField();
+        DataInicio = new javax.swing.JTextField();
+        filtrarBtn = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setClosable(true);
 
@@ -56,22 +75,59 @@ public class ExibirHistoricoUsoView extends javax.swing.JInternalFrame {
         ));
         jScrollPane1.setViewportView(tabelaHistorico);
 
+        DataFim.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DataFimActionPerformed(evt);
+            }
+        });
+
+        DataInicio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DataInicioActionPerformed(evt);
+            }
+        });
+
+        filtrarBtn.setText("Filtrar");
+        filtrarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                filtrarBtnActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Data de início:");
+
+        jLabel3.setText("Data de fim:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jSeparator1)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(330, 330, 330))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jSeparator1)
+                        .addContainerGap())
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(265, 315, Short.MAX_VALUE)
+                        .addComponent(jLabel1)
+                        .addGap(330, 330, 330))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(105, 105, 105)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(DataInicio, javax.swing.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(98, 98, 98)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(DataFim, javax.swing.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(filtrarBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(76, 76, 76))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(55, 55, 55)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 770, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(69, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -80,28 +136,63 @@ public class ExibirHistoricoUsoView extends javax.swing.JInternalFrame {
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 301, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(113, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(DataInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(DataFim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(filtrarBtn))
+                .addGap(50, 50, 50)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 297, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(120, 120, 120))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 556, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void DataFimActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DataFimActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DataFimActionPerformed
+
+    private void DataInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DataInicioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_DataInicioActionPerformed
+
+    private void filtrarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filtrarBtnActionPerformed
+
+    }//GEN-LAST:event_filtrarBtnActionPerformed
+
+    public void addFiltrarButtonActionListener(ActionListener actionListener) {
+        filtrarBtn.addActionListener(actionListener);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField DataFim;
+    private javax.swing.JTextField DataInicio;
+    private javax.swing.JButton filtrarBtn;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
